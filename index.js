c9c20e8dc63aaaf7a74414a524daf40ad06d8754
Victory <git@dfhu.org>
2016-06-26 06:37:30 -0400

    Clean up compile Promise logic

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var exec = require('child_process').exec;
 var fs = require('fs');
+
+var express = require('express');
+var app = express();
+
 var spawnOptions =  {
   'cwd': '/dev/shm'
 };
 var solcArgs = ' --combined-json abi,asm,ast,bin,bin-runtime,clone-bin,devdoc,interface,opcodes,userdoc ';
+
+
 var showCwd = function () {
   exec('pwd', spawnOptions, (error, stdout, stderr) => {
     if (error) {
@@ -18,35 +24,56 @@ var showCwd = function () {
   });
 };
 
-// PoC for running cli solc from node
-new Promise(function (resolve, reject) {
-  fs.writeFile(spawnOptions.cwd + "/foo.sol", "contract abstract {}", function(err) {
-    if (err) {
-      reject(err);
-      return;
-    }
-    resolve();
+var compile = function () {
+  // PoC for running cli solc from node
+  return new Promise(function (resolve, reject) {
+
+    // save the input to a temp file
+    fs.writeFile(spawnOptions.cwd + "/foo.sol", "contract abstract {}", function (err) {
+      if (err) {
+        reject(Promise.reject(err));
+        return;
+      }
+      resolve();
+    });
+  }).then(
+    function () {
+      // debug
+      console.log('done writting');
+      return true;
+    },
+    showCwd // note the cwd
+  ).then(function () {
+    // compile with solc
+    return new Promise(function (resolve, reject) {
+      exec(`solc foo.sol ${solcArgs}`, spawnOptions, (error, stdout, stderr) => {
+        if (error) {
+          reject(`exec error: ${error}`);
+          return;
+        }
+
+        if (stderr.length > 0) {
+          console.log(`stderr: ${stderr}`);
+        }
+
+        resolve(stdout);
+      });
+    });
   });
-}).then(
-  function () {
-    console.log('done writting');
-    return true;
+};
+
+app.get('/', function (req, res) {
+  compile().then(function (result) {
+    res.setHeader('Content-Type', 'application/json');
+    console.log(result);
+    res.send(result);
   },
-  showCwd
-).then(function (ok) {
-  if (!ok) {
-    return false;
-  }
-  exec(`solc foo.sol ${solcArgs}`, spawnOptions, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`cat foo.test: ${stdout}`);
-    if (stderr.length > 0) {
-      console.log(`stderr: ${stderr}`);
-    }
+  function (err) {
+    res.send(err);
   });
 });
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
